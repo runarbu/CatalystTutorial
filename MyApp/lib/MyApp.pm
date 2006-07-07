7f90aaded0726d5d7a95d15683bddc2cc8badfb9
hkclark <hkclark@4ad37cd2-5fec-0310-835f-b3785c72a374>
2006-07-07 18:09:51 +0000

    Final code for "Part 5 - Authorization" for Catalyst 5.7000

--- MyApp/lib/MyApp.pm
+++ MyApp/lib/MyApp.pm
@@ -23,6 +23,8 @@ use Catalyst qw/
         Authentication
         Authentication::Store::DBIC
         Authentication::Credential::Password
+        Authorization::Roles
+        Authorization::ACL
         
         Session
         Session::Store::FastMmap
@@ -30,7 +32,7 @@ use Catalyst qw/
         /;
 
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 # Configure the application. 
 #
@@ -45,6 +47,23 @@ __PACKAGE__->config( name => 'MyApp' );
 
 # Start the application
 __PACKAGE__->setup;
+
+
+# Authorization::ACL Rules
+__PACKAGE__->deny_access_unless(
+        "/books/form_create",
+        [qw/admin/],
+    );
+__PACKAGE__->deny_access_unless(
+        "/books/form_create_do",
+        [qw/admin/],
+    );
+__PACKAGE__->deny_access_unless(
+        "/books/delete",
+        [qw/user admin/],
+    );
+
+
 
 
 =head1 NAME
