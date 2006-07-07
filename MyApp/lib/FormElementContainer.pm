6abc910997dcfe093b23f888eb3f6ba0eff0cab6
hkclark <hkclark@4ad37cd2-5fec-0310-835f-b3785c72a374>
2006-07-07 18:12:58 +0000

    Final code for "Part 8 - Advanced CRUD" for Catalyst 5.7000

---
+++ MyApp/lib/FormElementContainer.pm
@@ -0,0 +1,22 @@
+package FormElementContainer;
+
+use base 'HTML::Widget::Container';
+
+sub _build_element {
+    my ($self, $element) = @_;
+
+    return () unless $element;
+    if (ref $element eq 'ARRAY') {
+        return map { $self->_build_element($_) } @{$element};
+    }
+    my $e = $element->clone;
+    my $class = $e->attr('class') || '';
+    $e = new HTML::Element('span', class => 'fields_with_errors')->push_content($e)
+        if $self->error && $e->tag eq 'input';
+
+    return $e ? ($e) : ();
+}
+
+1;
+
+
