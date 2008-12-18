86e879e324330a75caae8c901cabf4cc795a75d5
hkclark <hkclark@4ad37cd2-5fec-0310-835f-b3785c72a374>
2008-12-18 16:20:03 +0000

    Remove expanded file to avoid confusion with tarballs

--- trunk/examples/Tutorial/MyApp/myapp03.sql
+++
@@ -1,8 +0,0 @@
---
--- Convert passwords to SHA-1 hashes
---
-UPDATE users SET password = 'e727d1464ae12436e899a726da5b2f11d8381b26' WHERE id = 1;
-UPDATE users SET password = 'e727d1464ae12436e899a726da5b2f11d8381b26' WHERE id = 2;
-UPDATE users SET password = 'e727d1464ae12436e899a726da5b2f11d8381b26' WHERE id = 3;
-
-
