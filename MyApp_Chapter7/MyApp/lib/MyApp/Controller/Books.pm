5a0beb15dc0eac3988e769fafdc9ba2663ff545e
caelum <caelum@4ad37cd2-5fec-0310-835f-b3785c72a374>
2010-02-07 11:29:48 +0000

    update chapter 7 code (same as chapter 6)

--- MyApp_Chapter7/MyApp/lib/MyApp/Controller/Books.pm
+++ MyApp_Chapter7/MyApp/lib/MyApp/Controller/Books.pm
@@ -1,8 +1,8 @@
 package MyApp::Controller::Books;
+use Moose;
+use namespace::autoclean;
 
-use strict;
-use warnings;
-use parent 'Catalyst::Controller';
+BEGIN {extends 'Catalyst::Controller'; }
 
 =head1 NAME
 
@@ -16,7 +16,6 @@ Catalyst Controller.
 
 =cut
 
-
 =head2 index
 
 =cut
@@ -26,7 +25,6 @@ sub index :Path :Args(0) {
 
     $c->response->body('Matched MyApp::Controller::Books in Books.');
 }
-
 
 =head2 list
 
@@ -47,10 +45,8 @@ sub list : Local {
     # Set the TT template to use.  You will almost always want to do this
     # in your action methods (action methods respond to user input in
     # your controllers).
-    $c->stash->{template} = 'books/list.tt2';
+    $c->stash(template => 'books/list.tt2');
 }
-
-
 
 =head2 base
 
@@ -67,32 +63,6 @@ sub base :Chained('/') :PathPart('books') :CaptureArgs(0) {
     # Print a message to the debug log
     $c->log->debug('*** INSIDE BASE METHOD ***');
 }
-
-
-
-=head2 object
-
-Fetch the specified book object based on the book ID and store
-it in the stash
-
-=cut
-
-sub object :Chained('base') :PathPart('id') :CaptureArgs(1) {
-    # $id = primary key of book to delete
-    my ($self, $c, $id) = @_;
-
-    # Find the book object and store it in the stash
-    $c->stash(object => $c->stash->{resultset}->find($id));
-
-    # Make sure the lookup was successful.  You would probably
-    # want to do something like this in a real app:
-    #   $c->detach('/error_404') if !$c->stash->{object};
-    die "Book $id not found!" if !$c->stash->{object};
-
-    # Print a message to the debug log
-    $c->log->debug("*** INSIDE OBJECT METHOD for obj id=$id ***");
-}
-
 
 
 =head2 url_create
@@ -135,7 +105,6 @@ sub url_create :Chained('base') :PathPart('url_create') :Args(3) {
 }
 
 
-
 =head2 form_create
 
 Display form to collect information for book to create
@@ -148,7 +117,6 @@ sub form_create :Chained('base') :PathPart('form_create') :Args(0) {
     # Set the TT template to use
     $c->stash->{template} = 'books/form_create.tt2';
 }
-
 
 
 =head2 form_create_do
@@ -187,6 +155,29 @@ sub form_create_do :Chained('base') :PathPart('form_create_do') :Args(0) {
 }
 
 
+=head2 object
+
+Fetch the specified book object based on the book ID and store
+it in the stash
+
+=cut
+
+sub object :Chained('base') :PathPart('id') :CaptureArgs(1) {
+    # $id = primary key of book to delete
+    my ($self, $c, $id) = @_;
+
+    # Find the book object and store it in the stash
+    $c->stash(object => $c->stash->{resultset}->find($id));
+
+    # Make sure the lookup was successful.  You would probably
+    # want to do something like this in a real app:
+    #   $c->detach('/error_404') if !$c->stash->{object};
+    die "Book $id not found!" if !$c->stash->{object};
+
+    # Print a message to the debug log
+    $c->log->debug("*** INSIDE OBJECT METHOD for obj id=$id ***");
+}
+
 
 =head2 delete
 
@@ -202,7 +193,7 @@ sub delete :Chained('object') :PathPart('delete') :Args(0) {
         unless $c->stash->{object}->delete_allowed_by($c->user->get_object);
 
     # Use the book object saved by 'object' and delete it along
-    # with related 'book_authors' entries
+    # with related 'book_author' entries
     $c->stash->{object}->delete;
 
     # Use 'flash' to save information across requests until it's read
@@ -211,7 +202,6 @@ sub delete :Chained('object') :PathPart('delete') :Args(0) {
     # Redirect the user back to the list page
     $c->response->redirect($c->uri_for($self->action_for('list')));
 }
-
 
 
 =head2 list_recent
@@ -260,17 +250,13 @@ sub list_recent_tcp :Chained('base') :PathPart('list_recent_tcp') :Args(1) {
     $c->stash->{template} = 'books/list.tt2';
 }
 
-
-
-=head1 AUTHOR
-
-root
-
 =head1 LICENSE
 
-This library is free software, you can redistribute it and/or modify
+This library is free software. You can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
+__PACKAGE__->meta->make_immutable;
 
 1;
