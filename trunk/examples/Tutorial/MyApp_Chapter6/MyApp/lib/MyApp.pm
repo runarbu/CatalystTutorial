4c349176c8b102c15de6c7934d278c5981c948b8
caelum <caelum@4ad37cd2-5fec-0310-835f-b3785c72a374>
2010-02-07 11:38:23 +0000

    add flash_to_stash => 1 to chapter 5/6/7 code

--- trunk/examples/Tutorial/MyApp_Chapter6/MyApp/lib/MyApp.pm
+++ trunk/examples/Tutorial/MyApp_Chapter6/MyApp/lib/MyApp.pm
@@ -45,6 +45,7 @@ __PACKAGE__->config(
     name => 'MyApp',
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
+    session => { flash_to_stash => 1 },
 );
 
 # Configure SimpleDB Authentication
