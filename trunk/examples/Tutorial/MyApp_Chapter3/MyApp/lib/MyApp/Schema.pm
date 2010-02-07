6c140599ab697b6c3ad24ac0d0082899930f9339
caelum <caelum@4ad37cd2-5fec-0310-835f-b3785c72a374>
2010-02-07 08:11:47 +0000

    update Chapter 3 app

--- trunk/examples/Tutorial/MyApp_Chapter3/MyApp/lib/MyApp/Schema.pm
+++ trunk/examples/Tutorial/MyApp_Chapter3/MyApp/lib/MyApp/Schema.pm
@@ -1,15 +1,20 @@
 package MyApp::Schema;
 
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
 use strict;
 use warnings;
 
 use base 'DBIx::Class::Schema';
 
-__PACKAGE__->load_namespaces;
+__PACKAGE__->load_namespaces(
+    result_namespace => 'Result',
+);
 
 
-# Created by DBIx::Class::Schema::Loader v0.04006 @ 2009-11-15 00:41:13
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:QgAQ8FI0SzZrHfnfiPmtcQ
+# Created by DBIx::Class::Schema::Loader v0.05001 @ 2010-02-07 03:08:05
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:8RFCrYpX8XplI+2rG0nYXg
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
