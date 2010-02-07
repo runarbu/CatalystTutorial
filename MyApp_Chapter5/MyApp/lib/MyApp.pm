a1799b9ad1faafa82b1438ad639ac0961a131828
caelum <caelum@4ad37cd2-5fec-0310-835f-b3785c72a374>
2010-02-07 10:28:13 +0000

    update chapter 5

--- MyApp_Chapter5/MyApp/lib/MyApp.pm
+++ MyApp_Chapter5/MyApp/lib/MyApp.pm
@@ -1,7 +1,6 @@
 package MyApp;
-
-use strict;
-use warnings;
+use Moose;
+use namespace::autoclean;
 
 use Catalyst::Runtime 5.80;
 
@@ -13,22 +12,24 @@ use Catalyst::Runtime 5.80;
 # Static::Simple: will serve static files from the application's root
 #                 directory
 
-use parent qw/Catalyst/;
-# Load plugins
 use Catalyst qw/
-                -Debug
-                ConfigLoader
-                Static::Simple
+    -Debug
+    ConfigLoader
+    Static::Simple
 
-                StackTrace
+    StackTrace
 
-                Authentication
+    Authentication
 
-                Session
-                Session::Store::FastMmap
-                Session::State::Cookie
-                /;
+    Session
+    Session::Store::FastMmap
+    Session::State::Cookie
+/;
+
+extends 'Catalyst';
+
 our $VERSION = '0.01';
+$VERSION = eval $VERSION;
 
 # Configure the application.
 #
@@ -40,10 +41,10 @@ our $VERSION = '0.01';
 # local deployment.
 
 __PACKAGE__->config(
-        name    => 'MyApp',
-        session => {flash_to_stash => 1},
-    );
-
+    name => 'MyApp',
+    # Disable deprecated behavior needed by old applications
+    disable_component_resolution_regex_fallback => 1,
+);
 
 # Configure SimpleDB Authentication
 __PACKAGE__->config->{'Plugin::Authentication'} = {
@@ -73,10 +74,6 @@ MyApp - Catalyst based application
 =head1 SEE ALSO
 
 L<MyApp::Controller::Root>, L<Catalyst>
-
-=head1 AUTHOR
-
-root
 
 =head1 LICENSE
 
