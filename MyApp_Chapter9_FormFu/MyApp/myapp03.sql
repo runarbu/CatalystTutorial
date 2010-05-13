868c1d8abdd62f20b29e1f7bba4dcb282ab2b6a3
caelum <caelum@4ad37cd2-5fec-0310-835f-b3785c72a374>
2010-05-13 12:56:04 +0000

    change created/updated columns to type "timestamp"

--- MyApp_Chapter9_FormFu/MyApp/myapp03.sql
+++ MyApp_Chapter9_FormFu/MyApp/myapp03.sql
@@ -1,6 +1,6 @@
 --
 -- Add 'created' and 'updated' columns to 'book' table.
 --
-ALTER TABLE book ADD created INTEGER;
-ALTER TABLE book ADD updated INTEGER;
+ALTER TABLE book ADD created TIMESTAMP;
+ALTER TABLE book ADD updated TIMESTAMP;
 UPDATE book SET created = DATETIME('NOW'), updated = DATETIME('NOW');
