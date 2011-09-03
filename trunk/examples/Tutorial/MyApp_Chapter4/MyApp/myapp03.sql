09a138e2e65d4da799a353d8bb033ffb49c1c50f
hkclark <hkclark@4ad37cd2-5fec-0310-835f-b3785c72a374>
2011-09-03 19:35:03 +0000

    Update for v5.9002 of Catalyst::Manual

--- trunk/examples/Tutorial/MyApp_Chapter4/MyApp/myapp03.sql
+++
@@ -1,6 +0,0 @@
---
--- Add 'created' and 'updated' columns to 'book' table.
---
-ALTER TABLE book ADD created TIMESTAMP;
-ALTER TABLE book ADD updated TIMESTAMP;
-UPDATE book SET created = DATETIME('NOW'), updated = DATETIME('NOW');
