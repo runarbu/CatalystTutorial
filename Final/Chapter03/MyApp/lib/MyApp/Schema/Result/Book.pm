fdb1dbb838380f3b90a6ddc4a97e0bb8f7a073f4
hkclark <hkclark@4ad37cd2-5fec-0310-835f-b3785c72a374>
2011-09-03 19:35:03 +0000

    Update for v5.9002 of Catalyst::Manual

---
+++ Final/Chapter03/MyApp/lib/MyApp/Schema/Result/Book.pm
@@ -0,0 +1,88 @@
+package MyApp::Schema::Result::Book;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use namespace::autoclean;
+extends 'DBIx::Class::Core';
+
+__PACKAGE__->load_components("InflateColumn::DateTime");
+
+=head1 NAME
+
+MyApp::Schema::Result::Book
+
+=cut
+
+__PACKAGE__->table("book");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 title
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 rating
+
+  data_type: 'integer'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "title",
+  { data_type => "text", is_nullable => 1 },
+  "rating",
+  { data_type => "integer", is_nullable => 1 },
+);
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 book_authors
+
+Type: has_many
+
+Related object: L<MyApp::Schema::Result::BookAuthor>
+
+=cut
+
+__PACKAGE__->has_many(
+  "book_authors",
+  "MyApp::Schema::Result::BookAuthor",
+  { "foreign.book_id" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2011-08-30 18:09:34
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:t/qfcGk8ZG6YnFIo+3dH3w
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+
+# many_to_many():
+#   args:
+#     1) Name of relationship bridge, DBIC will create accessor with this name
+#     2) Name of has_many() relationship this many_to_many() is shortcut for
+#     3) Name of belongs_to() relationship in model class of has_many() above
+#   You must already have the has_many() defined to use a many_to_many().
+__PACKAGE__->many_to_many(authors => 'book_authors', 'author');
+
+
+__PACKAGE__->meta->make_immutable;
+1;
--- MyApp_Chapter6/MyApp/t/controller_Logout.t
+++
@@ -1,9 +0,0 @@
-use strict;
-use warnings;
-use Test::More;
-
-BEGIN { use_ok 'Catalyst::Test', 'MyApp' }
-BEGIN { use_ok 'MyApp::Controller::Logout' }
-
-ok( request('/logout')->is_success, 'Request should succeed' );
-done_testing();
